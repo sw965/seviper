21ef1ee355ace440bf49b01c82dab34850f365b5
sw965 <tyuusotunamapoaig@gmail.com>
2022-02-05 19:12:03 +0900

    setup.pyの追加

--- seviper/ai.py
+++ seviper/ai.py
@@ -3,10 +3,12 @@ import itertools
 import numpy as np
 import parts
 
-ALL_BASE_STATES = [state + 1 for state in range(parts.MAX_BASE_STATE)]
-ALL_MOVE_NAMES = ["なし"] + parts.ALL_MOVE_NAMES
-ALL_MOVE_POWERS = [power + 1 for power in range(parts.MAX_MOVE_POWER)]
-ALL_ACCURACY = [i + 1 for i in range(100)]
+POKE_NAME_FEATURES = ["なし"] + parts.RATE_POKE_NAMES
+BASE_STATE_FEATURES = [state + 1 for state in range(parts.MAX_BASE_STATE)]
+MOVE_NAME_FEATURES = ["なし"] + parts.ALL_MOVE_NAMES
+MOVE_POWER_FEATURES = [power + 1 for power in range(parts.MAX_MOVE_POWER)]
+ACCURACY_FEATURES = [i + 1 for i in range(100)]
+ITEM_FEATURES = ["なし"] + parts.ALL_ITEMS
 
 def binary_image_team_pokemon_2d_size():
     height = 0
@@ -41,19 +43,19 @@ class BinaryImageTeamPokemon:
 
     INDEX_1D_TO_2D = (lambda height, width:[(h, w) for h in range(height) for w in range(width)])(HEIGHT, WIDTH)
 
-    POKE_NAME_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.RATE_POKE_NAMES, False)
-    TYPE_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.RATE_POKE_NAMES, False)
+    POKE_NAME_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, POKE_NAME_FEATURES, False)
+    TYPE_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_TYPES, False)
     LEARNSET_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_MOVE_NAMES, False),
-    BASE_STATE_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, ALL_BASE_STATES, True),
+    BASE_STATE_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, BASE_STATE_FEATURES, True),
 
     NATURE_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_NATURES, False),
     ABILITY_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_ABILITIES, False),
     GENDER_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_GENDERS, False),
-    ITEM_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_ITEMS, False)
+    ITEM_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, ITEM_FEATURES, False)
 
-    MOVE_NAME_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, ALL_MOVE_NAMES, False)
-    MOVE_POWER_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, ALL_MOVE_POWERS, True)
-    MOVE_ACCURACY_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, ALL_ACCURACY, True)
+    MOVE_NAME_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, MOVE_NAME_FEATURES, False)
+    MOVE_POWER_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, MOVE_POWER_FEATURES, True)
+    MOVE_ACCURACY_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, ACCURACY_FEATURES, True)
     POINT_UP_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_POINT_UPS, True),
     INDIVIDUAL_VALUE_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_INDIVIDUAL_VALUES, True)
     EFFORT_VALUE_1D_INDEX_TO_2D = make_feature_1d_index_to_2d(INDEX_1D_TO_2D, parts.ALL_EFFORT_VALUES, True)
@@ -78,7 +80,9 @@ class BinaryImageTeamPokemon:
         TYPE_1D_INDEX_TO_2D,
         MOVE_POWER_1D_INDEX_TO_2D,
         MOVE_ACCURACY_1D_INDEX_TO_2D,
+        #半回復技かどうかの特徴量
         None,
+        #一撃必殺技かどうかの特徴量
         None,
 
         MOVE_NAME_1D_INDEX_TO_2D,
@@ -196,10 +200,10 @@ class BinaryImageTeamPokemon:
         self.ability = None
         self.gender = None
         self.item = None
-        self.move_names = []
-        self.point_ups = []
-        self.individual = None
-        self.individual = None
+        self.move_names = [None, None, None, None]
+        self.point_ups = [None, None, None, None]
+        self.individual = Individual(None, None, None, None, None, None)
+        self.effort = Effort(None, None, None, None, None, None)
 
     def print_2d(self, depth):
         height = BinaryImageTeamPokemon.HEIGHT
@@ -250,26 +254,29 @@ class BinaryImageTeamPokemon:
     def set_nature(self, nature):
         self.set_features(parts.ALL_NATURES, [nature], BinaryImageTeamPokemon.NATURE_DEPTH,
                           "性格を再度入力しようとした")
+        self.nature = nature
 
     def set_ability(self, ability):
         assert self.name is not None
         assert ability in POKEDEX[self.name].all_abilities
         self.set_features(parts.ALL_ABILITIES, [ability], BinaryImageTeamPokemon.ABILITY_DEPTH,
                           "特性を再度入力しようとした")
+        self.ability = ability
 
     def set_gender(self, gender):
         assert self.name is not None
         assert gender in valid_genders(poke_name, gender)
         self.set_features(parts.ALL_GENDERS, [gender], BinaryImageTeamPokemon.GENDER_DEPTH,
                           "性別を再度入力しようとした")
+        self.gender = gender
 
     def set_item(self, item):
         self.set_features(parts.ALL_ITEMS, [item], BinaryImageTeamPokemon.ITEM_DEPTH,
                           "アイテムを再度入力しようとした")
+        self.item = item
 
     def set_move_name(self, move_name, depth):
         poke_data = POKEDEX[poke_name]
-
         assert move_name in ALL_MOVE_NAMES
 
         depth_index = [MOVE1_NAME_DEPTH, MOVE2_NAME_DEPTH, MOVE3_NAME_DEPTH, MOVE4_NAME_DEPTH].index(depth)
@@ -316,22 +323,48 @@ class BinaryImageTeamPokemon:
     def set_move1_name(self, move_name):
         assert move_name != "なし"
         self.set_move_name(move_name, BinaryImageTeamPokemon.MOVE1_NAME_DEPTH)
+        assert self.move_names.count(None) == 4
+        self.move_names[0] = move_name
 
     def set_move2_name(self, move_name):
         self.set_move_name(move_name, BinaryImageTeamPokemon.MOVE2_NAME_DEPTH)
+        assert self.move_names.count(None) == 3
+        self.move_names[1] = move_name
 
     def set_move3_name(self, move_name):
         self.set_move_name(move_name, BinaryImageTeamPokemon.MOVE3_NAME_DEPTH)
+        assert self.move_names.count(None) == 2
+        self.move_names[2] = move_name
 
     def set_move4_name(self, move_name):
         self.set_move_name(move_name, BinaryImageTeamPokemon.MOVE4_NAME_DEPTH)
+        assert self.move_names.count(None) == 1
+        self.move_names[3] = move_name
 
     def set_point_up(self, point_up, depth):
-        assert is_valid_point_up(point_up)
-        assert self.count(1, BinaryImageTeamPokemon.MOVE_NAMES_DEPTH) == MAX_MOVESET_NUM
-
+        assert parts.is_valid_point_up(point_up)
         self.set_features(parts.ALL_POINT_UPS, [point_up], BinaryImageTeamPokemon.POINT_UP_DEPTH,
                           "ポイントアップを再度入力しようとした")
+
+    def set_point_up1(self, point_up):
+        self.set_point_up(point_up, BinaryImageTeamPokemon.POINT_UP1_DEPTH)
+        assert self.move_names[0] is not None
+        self.point_ups[0] = point_up
+
+    def set_point_up2(self, point_up):
+        self.set_point_up(point_up, BinaryImageTeamPokemon.POINT_UP2_DEPTH)
+        assert self.move_names[1] is not None
+        self.point_ups[1] = point_up
+
+    def set_point_up3(self, point_up):
+        self.set_point_up(point_up, BinaryImageTeamPokemon.POINT_UP3_DEPTH)
+        assert self.move_names[2] is not None
+        self.point_ups[2] = point_up
+
+    def set_point_up4(self, point_up):
+        self.set_point_up(point_up, BinaryImageTeamPokemon.POINT_UP4_DEPTH)
+        assert self.move_names[3] is not None
+        self.point_ups[3] = point_up
 
     def set_individual_v(self, individual_v, depth, state_type):
         assert individual_v in ALL_INDIVIDUAL_VALUES
@@ -339,42 +372,91 @@ class BinaryImageTeamPokemon:
 
     def set_individual_hp(self, individual_v):
         self.set_individual_v(individual_v, BinaryImageTeamPokemon.INDIVIDUAL_HP_DEPTH, "HP")
+        self.individual.hp = individual_v
 
     def set_individual_atk(self, individual_v):
         self.set_individual_v(individual_v, BinaryImageTeamPokemon.INDIVIDUAL_ATK_DEPTH, "攻撃")
+        self.individual.atk = individual_v
 
     def set_individual_def(self, individual_v):
         self.set_individual_v(individual_v, BinaryImageTeamPokemon.INDIVIDUAL_DEF_DEPTH, "防御")
+        self.individual.defe = individual_v
 
     def set_individual_sp_atk(self, individual_v):
         self.set_individual_v(individual_v, BinaryImageTeamPokemon.INDIVIDUAL_SP_ATK_DEPTH, "特攻")
+        self.individual.sp_atk = individual_v
 
     def set_individual_sp_def(self, individual_v):
         self.set_individual_v(individual_v, BinaryImageTeamPokemon.INDIVIDUAL_SP_DEF_DEPTH, "特防")
+        self.individual.sp_def = individual_v
 
     def set_individual_speed(self, individual_v):
         self.set_individual_v(individual_v, BinaryImageTeamPokemon.INDIVIDUAL_SPEED_DEPTH, "素早さ")
+        self.individual.speed = individual_v
 
     def set_effort_v(self, effort_v, depth, state_type):
         assert effort_v in parts.ALL_EFFORT_VALUES
+
+        if self.effort.hp is None:
+            hp = 0
+        if self.effort.atk is None:
+            atk = 0
+        if self.effort.defe is None:
+            defe = 0
+        if self.effort.sp_atk is None:
+            sp_atk = 0
+        if self.effort.sp_def is None:
+            sp_def = 0
+        if self.effort.speed is None:
+            speed = 0
+
+        sum_v = hp + atk + defe + sp_atk + sp_def + speed
+        assert (effort_v + sum_v) <= Effort.MAX_SUM, "努力値の合計値が" + Effort.MAX_SUM + "を超えた"
         self.set_features(parts.ALL_EFFORT_VALUES, [effort_v], depth, "努力値" + state_type + "を再入力した")
 
     def set_effort_hp(self, effort_v):
         self.set_effort_v(effort_v, BinaryImageTeamPokemon.EFFORT_HP_DEPTH, "HP")
+        self.effort.hp = effort_v
 
     def set_effort_atk(self, effort_v):
         self.set_effort_v(effort_v, BinaryImageTeamPokemon.EFFORT_ATK_DEPTH, "攻撃")
+        self.effort.atk = effort_v
 
     def set_effort_def(self, effort_v):
         self.set_effort_v(effort_v, BinaryImageTeamPokemon.EFFORT_DEF_DEPTH, "防御")
+        self.effort.defe = effort_v
 
     def set_effort_sp_atk(self, effort_v):
         self.set_effort_v(effort_v, BinaryImageTeamPokemon.EFFORT_SP_ATK_DEPTH, "特攻")
+        self.effort.sp_atk = effort_v
 
     def set_effort_sp_def(self, effort_v):
         self.set_effort_v(effort_v, BinaryImageTeamPokemon.EFFORT_SP_DEF_DEPTH, "特防")
+        self.effort.sp_def = effort
 
     def set_effort_speed(self, effort_v):
         self.set_effort_v(effort_v, BinaryImageTeamPokemon.EFFORT_SPEED_DEPTH, "素早さ")
+        self.effort.speed = effort_v
 
-binary_image_team_pokemon = BinaryImageTeamPokemon()
+
+class BinaryImageTeam:
+    def __init__(self):
+        self.images = [BinaryImageTeamPokemon() for _ in range(MAX_TEAM_NUM)]
+
+    def get_data(self):
+        return sum([image.data for image in self.images])
+
+    def set_poke_name(self, poke_name, index):
+        if index in [0, 1, 2]:
+            assert poke_name != "なし"
+
+        poke_names = [image.poke_name for image in self.images]
+        assert poke_name not in poke_names, poke_name + " は既にチームに存在する(同じ名前のポケモンを入れようとした)"
+
+        images[index].set_poke_name(poke_name)
+
+    def set_item(self, item, index):
+        items = [image.item for image in self.images]
+        if item != "なし":
+            assert item not in items, item + " は別のポケモンが既に持っている"
+        self.images[index].set_item(item)
